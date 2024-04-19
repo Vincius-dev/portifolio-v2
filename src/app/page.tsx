97c19bc844185e064d791e296ccb8ec9b19db7f9
vinicius-dev <viniciusgabfarias123@gmail.com>
2024-04-19 15:30:01 -0300

    feat: inicio do conteudo do home.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,25 @@ import { motion } from "framer-motion";
 
 export default function Home() {
   return (
-
     <motion.div
       className="h-full"
       initial={{ y: "-200vh" }}
       animate={{ y: "0%" }}
       transition={{ duration: 1 }}
     >
-      <main className="flex flex-col">
-        <div className="1/2 relative"></div>
-        <div className="1/2"></div>
+      <main className="flex justify-center flex-col items-center mt-4">
+          <div className="w-6/6 flex"> {/* Add the "flex" class here */}
+            <div className="w-3/6">
+              <h1 className="text-2xl font-bold mb-4 text-left">Vinícius Gabriel</h1>
+            </div>
+            <div className="w-3/6">
+              teste
+            </div>
+          </div>
+
+          <div>
+            <h3 className="mt-4"><b className="underline">Back-End Developer</b> - Um profissional completo. Com conhecimentos em <b className="underline">Front</b>, <b className="underline">Redes</b> e <b className="underline">Devops</b></h3>
+          </div>
       </main>
     </motion.div>
   );
