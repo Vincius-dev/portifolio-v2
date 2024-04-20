8a21b97bce78e34a6bf73fc496ad6fa7efa76074
vinicius-dev <viniciusgabfarias123@gmail.com>
2024-04-20 15:01:08 -0300

    fix: button with framer.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,8 +9,8 @@ export default function Home() {
   return (
     <motion.div
       className="h-full"
-      initial={{ y: "-200vh" }}
-      animate={{ y: "0%" }}
+      initial={{ x: "-200vw" }}
+      animate={{ x: "0%" }}
       transition={{ duration: 1 }}
     >
       <main className="flex justify-center flex-col items-center mt-10 px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
@@ -22,11 +22,19 @@ export default function Home() {
             <h1 className="xl:text-5xl sm:text-lg md:text-2xl font-bold m-4 text-left">Vinícius Gabriel</h1>
             <h3 className="m-4 text-left"><b className="underline dark:text-purple-600">Back-End Developer</b> - Java / Spring Boot</h3>
             <h3 className="m-4 text-left">Sou um <b className="underline dark:text-purple-600">profissional completo</b>, além de back-end<p></p> possuo conhecimentos em <b className="underline dark:text-purple-600">Front</b>, <b className="underline dark:text-purple-600">Redes</b> e <b className="underline dark:text-purple-600">Devops</b></h3>
-            <h3 className="m-4 text-left">Conheça meus projetos: <Button><Link href="/projects">Projetos</Link></Button>
-          </h3>
+            <h3 className="m-4 text-left">Conheça meus projetos <motion.div
+              className="inline-block"
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9 }}
+              transition={{ type: "spring", stiffness: 300 }}
+              >
+                <Button>
+                  <Link href="/projects">Projetos</Link>
+                </Button>
+              </motion.div></h3>
+          </div>
         </div>
-      </div>
-    </main>
-    </motion.div >
+      </main>
+    </motion.div>
   );
 }
