65a161a0258b7cd02d9bb5e0b068edcac3aaa490
vinicius-dev <viniciusgabfarias123@gmail.com>
2024-04-21 18:07:11 -0300

    fix: z-index of transition provider.

--- src/components/TransitionProvider.tsx
+++ src/components/TransitionProvider.tsx
@@ -21,14 +21,14 @@ const TransitionProvider: React.FC<TransitionProviderProps> = ({ children, local
         <AnimatePresence mode="wait">
             <div key={pathName} className="w-screen h-screen">
                 <motion.div
-                    className="w-screen h-screen fixed bg-black dark:bg-purple-700 rounded-b-[100px] z-40"
+                    className="w-screen h-screen fixed bg-black dark:bg-purple-700 rounded-b-[100px] z-30"
                     animate={{ height: "0vh" }}
                     exit={{ height: "140vh" }}
                     transition={{ duration: 1, ease: "easeOut" }}
                 />
 
                 <motion.div
-                    className="fixed m-auto top-0 bottom-0 left-0 right-0 text-white text-8xl cursor-default z-50 w-fit h-fit"
+                    className="fixed m-auto top-0 bottom-0 left-0 right-0 text-white text-8xl cursor-default z-40 w-fit h-fit"
                     initial={{ opacity:1 }}
                     animate={{ opacity:0 }}
                     exit={{ opacity:0 }}
@@ -38,7 +38,7 @@ const TransitionProvider: React.FC<TransitionProviderProps> = ({ children, local
                 </motion.div>
 
                 <motion.div
-                    className="w-screen h-screen fixed dark:bg-purple-700 rounded-t-[100px] bottom-0 z-30"
+                    className="w-screen h-screen fixed dark:bg-purple-700 rounded-t-[100px] bottom-0 z-20"
                     initial={{ height: "140vh" }}
                     animate={{ height:"0vh"}}
                     transition={{ duration: 1, delay:0.5, ease: "easeOut" }}
