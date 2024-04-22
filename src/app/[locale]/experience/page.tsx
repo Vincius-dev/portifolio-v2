593c2f58d38d2e210372966d56efee1fec853115
vinicius-dev <viniciusgabfarias123@gmail.com>
2024-04-22 11:29:36 -0300

    feat: timeline and timecards, dark theme styles.

--- src/app/[locale]/experience/page.tsx
+++ src/app/[locale]/experience/page.tsx
@@ -1,5 +1,8 @@
 "use client"
 
+import TimeCertCard from "@/components/timeline/TimeCertCard";
+import TimeCourseCard from "@/components/timeline/TimeCourseCard";
+import TimeJobCard from "@/components/timeline/TimeJobCard";
 import { motion } from "framer-motion";
 import Image from "next/image";
 
@@ -11,41 +14,40 @@ const ExperiencePage = () => {
             animate={{ y: "0%" }}
             transition={{ duration: 1 }}
         >
-            <main className="flex justify-center flex-col items-center mt-10 px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
-                <ol className="relative lg:inset-x-40 md:lg:inset-x-40 inset-x-12 border-s  border-gray-200 dark:border-gray-700 rounded-lg">
-                    <li className="mb-10 border-x-y-4 ms-6">
-                        <span className="absolute flex items-center justify-center w-8 h-8 bg-blue-100 rounded-full -start-3 ring-8 ring-dark dark:ring-purple-800 dark:bg-purple-300">
-                            <Image src="/icons/java.png" alt="tech-image" width={64} height={64} />
-                        </span>
-                        <h3 className="flex ml-4 items-center mb-1 text-md font-semibold text-gray-900 dark:text-white">
-                            Desenvolvedor Java Jr
-                            <span className="bg-blue-100 text-blue-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-purple-700 dark:text-white ms-3">
-                                Atual
-                            </span>
-                        </h3>
-                        <time className="block mb-2 ml-4 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-                            JGM Contabil - 2024 até o momento
-                        </time>
-                        <p className="mb-4 text-justify w-3/6 font-normal text-gray-500 dark:text-gray-400">
-                            Desenvolvimento de aplicações Java, manutenção de sistemas, desenvolvimento de relatórios e integrações.
-                        </p>
-                    </li>
-                    <li className="mb-10  ms-6">
-                        <span className="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -start-3 ring-8 ring-white dark:ring-purple-800 dark:bg-purple-600"></span>
-                        <h3 className="mb-1 text-lg font-semibold text-gray-900 dark:text-white">Analista de Suporte</h3>
-                        <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">GinfoWork Informatica - 2022 - 2023</time>
-                        <p className="text-justify font-normal w-3/6 text-gray-500 dark:text-gray-400">All of the pages and components are first designed in Figma and we keep a parity between the two versions even as we update the project.</p>
-                    </li>
-                    <li className="ms-6">
-                        <span className="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -start-3 ring-8 ring-white dark:ring-purple-800 dark:bg-purple-600">
-                            <svg className="w-2.5 h-2.5 text-blue-800 dark:text-blue-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                            </svg>
-                        </span>
-                        <h3 className="mb-1 text-lg font-semibold text-gray-900 dark:text-white">Flowbite Library v1.2.2</h3>
-                        <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">Released on December 2nd, 2021</time>
-                        <p className="text-justify font-normal w-3/6 text-gray-500 dark:text-gray-400">Get started with dozens of web components and interactive elements built on top of Tailwind CSS.</p>
-                    </li>
-                    
+            <main className=" mt-10 px-4 pb-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+                <ol className="relative lg:inset-x-40 md:lg:inset-x-44 inset-x-12 border-s-8 border-black dark:border-purple-800 rounded-lg">
+                    <TimeJobCard
+                        title="Java Developer - Jr"
+                        subtitle="JGM Empresarial - 2024"
+                        icon="/icons/java.png"
+                        year="2024"
+                        description="Certificação de especialista em Azure, com foco em desenvolvimento de aplicações e integrações."
+                        isFirst={true}
+                    />
+                    <TimeCertCard
+                        title="Azure Expert XXX"
+                        subtitle="Azure - 2024"
+                        icon="/icons/azure.png"
+                        year="2024"
+                        description="Certificação de especialista em Azure, com foco em desenvolvimento de aplicações e integrações." 
+                        isFirst={false}
+                    />
+                    <TimeJobCard
+                        title="Java Developer - Jr"
+                        subtitle="JGM Empresarial - 2024"
+                        icon="/icons/java.png"
+                        year="2024"
+                        description="Certificação de especialista em Azure, com foco em desenvolvimento de aplicações e integrações."
+                        isFirst={false}
+                    />
+                    <TimeCourseCard 
+                        title="Azure Expert XXX"
+                        subtitle="Azure - 2024"
+                        icon="/icons/azure.png"
+                        year="2024"
+                        description="Certificação de especialista em Azure, com foco em desenvolvimento de aplicações e integrações." 
+                        isFirst={false}
+                    />
                 </ol>
             </main>
         </motion.div>
---
+++ src/components/timeline/TimeCertCard.tsx
@@ -0,0 +1,52 @@
+"use client"
+
+import { useTranslations } from "next-intl";
+import Image from "next/image";
+
+interface CardProps {	
+    title: string;
+    subtitle: string;
+    icon: string;
+    year: string;
+    description: string;
+    isFirst: boolean;
+}
+
+const TimeCertCard: React.FC<CardProps> = ({ title, subtitle, icon, year, description, isFirst }) => {
+
+    const e = useTranslations('experience');
+
+    return (
+        <li className="mb-10 w-4/6 p-3 rounded-e-2xl bg-indigo-900">
+        <span className="absolute flex items-center justify-center w-8 h-8 bg-blue-100 rounded-full -start-5 ring-8 ring-dark dark:ring-indigo-800 dark:bg-purple-300">
+            <Image src={icon} alt="tech-image" width={64} height={64} />
+            <p className="absolute text-md mr-28 font-semibold dark:text-white dark:bg-indigo-700 rounded-xl p-2">{year}</p>
+        </span>
+        <h3 className="ml-4 mb-1 text-lg underline font-extrabold text-gray-900 dark:text-indigo-600">
+            <div className="flex justify-between items-center w-full">
+                <span>
+                    {title}
+                    {isFirst && (
+                        <span className="bg-blue-100 text-blue-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-indigo-700 dark:text-white ms-3">
+                            {e('current')}
+                        </span>
+                    )}
+                </span>
+                <div className="relative flex justify-between items-end">
+                    <span className="bg-blue-100 text-blue-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-indigo-700 dark:text-white ms-3">
+                        {e('certification')}
+                    </span>
+                </div>
+            </div>
+        </h3>
+        <time className="block mb-2 ml-4 text-sm font-normal leading-none text-gray-400 dark:text-gray-300">
+            {subtitle}
+        </time>
+        <p className="mb-4 ml-4 text-justify text-md font-normal text-gray-500 dark:text-white">
+            {description}
+        </p>
+    </li>
+    )
+}
+
+export default TimeCertCard;
