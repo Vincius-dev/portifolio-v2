52d7da77d8a6bf29db5c839eb24bd0485342b1dc
vinicius-dev <viniciusgabfarias123@gmail.com>
2024-04-22 16:08:55 -0300

    feat: projetcs content.

--- src/app/[locale]/projects/page.tsx
+++ src/app/[locale]/projects/page.tsx
@@ -23,11 +23,58 @@ const ProjectsPage = () => {
                     prodLink="https://linear-systems-calc.vercel.app/calc/linear-system"
                     technologies={["/icons/ts.png", "/icons/next.png", "/icons/react.png"]}
                 />
-                
+                <ProjectComponent
+                    title={p('bot-notification-title')} 
+                    about={p('bot-notification-description')} 
+                    image="/projects/bot-notification.png" 
+                    repoLink="https://github.com/Vincius-dev/bot-notification"
+                    prodLink=""
+                    technologies={["/icons/python.png", "/icons/docker.png", "/icons/mongodb.png"]}
+                />
+                <ProjectComponent
+                    title={p('uno-java-title')} 
+                    about={p('uno-java-description')} 
+                    image="/projects/uno-java.png" 
+                    repoLink="https://github.com/Vincius-dev/uno-java"
+                    prodLink=""
+                    technologies={["/icons/java.png", "/icons/linux.png"]}
+                />
+                <ProjectComponent
+                    title={p('portifolio-v2-title')} 
+                    about={p('portifolio-v2-description')} 
+                    image="/projects/portfolio-v2.png" 
+                    repoLink="https://github.com/Vincius-dev/portifolio-v2/"
+                    prodLink="https://www.vinicius-dev.com/"
+                    technologies={["/icons/next.png", "/icons/react.png", "/icons/ts.png", "/icons/tailwind.png"]}
+                />
+                <ProjectComponent
+                    title={p('portifolio-v1-title')} 
+                    about={p('portifolio-v1-description')} 
+                    image="/projects/portfolio-v1.png" 
+                    repoLink="https://github.com/Vincius-dev/site-portifolio"
+                    prodLink="portifolio-viniciusg.vercel.app/"
+                    technologies={["/icons/js.png", "/icons/react.png"]}
+                />
+                <ProjectComponent
+                    title={p('corona-vapo-title')} 
+                    about={p('corona-vapo-description')} 
+                    image="/projects/corona-vapo.jpeg" 
+                    repoLink="https://github.com/Vincius-dev/Game-CoronaVapo"
+                    prodLink="https://github.com/Vincius-dev/Game-CoronaVapo/tree/Main1/Executaveis"
+                    technologies={[]}
+                />
+                <ProjectComponent
+                    title={p('medplus-title')} 
+                    about={p('medplus-description')} 
+                    image="/projects/medplus.png" 
+                    repoLink="https://github.com/Vincius-dev/usjt_medplus_project_frontend"
+                    prodLink="https://www.youtube.com/watch?v=gfEEJ9Umv1Q"
+                    technologies={["/icons/node.png", "/icons/ts.png", "/icons/angular.png", "/icons/postgress.png"]}
+                />
                 <ProjectComponent 
                     title={p('party-manager-title')} 
                     about={p('party-manager-description')} 
-                    image="/projects/imagem-exemplo.png" 
+                    image="/projects/festa-aniversario.png" 
                     repoLink="https://github.com/Vincius-dev/linear-systems-calc"
                     prodLink="https://linear-systems-calc.vercel.app/calc/linear-system"
                     technologies={["/icons/java.png", "/icons/spring-boot.png", "/icons/react.png", "/icons/next.png",
