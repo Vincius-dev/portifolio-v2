b4bece4f28f243975b51e1bc6ef93c5928641e6b
vinicius-dev <viniciusgabfarias123@gmail.com>
2024-04-23 23:20:17 -0300

    style: profile picture update.

--- src/app/[locale]/home/page.tsx
+++ src/app/[locale]/home/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
       <main className="flex justify-center flex-col items-center mt-10 px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
         <div className="w-full flex flex-col sm:flex-row justify-center items-center">
           <div className="w-2/5 items-center sm:w-full">
-            <Image src="/perfil.jpeg" alt="Profile Picture" width={450} height={450} className="rounded-2xl lg:mx-auto md:lg:mx-auto sm:n-2" /> {/* Add mx-auto class to center the image */}
+            <Image src="/perfil.png" alt="Profile Picture" width={450} height={450} className="rounded-2xl lg:mx-auto md:lg:mx-auto sm:n-2" /> {/* Add mx-auto class to center the image */}
           </div>
           <div className="w-4/5 sm:w-4/5 text-lg md:text-xl xl:text-2xl sm:text-sm">
             <Reveal>
